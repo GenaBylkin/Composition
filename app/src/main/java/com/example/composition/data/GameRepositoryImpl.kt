d3f6fdf3353acafb8df25f523bbbac60da5dda61
GenaBylkin <anton.nemanixin@mail.ru>
2023-05-11 19:22:22 +0300

    nastr

--- app/src/main/java/com/example/composition/data/GameRepositoryImpl.kt
+++ app/src/main/java/com/example/composition/data/GameRepositoryImpl.kt
@@ -4,10 +4,26 @@ import com.example.composition.domain.entity.GameSettings
 import com.example.composition.domain.entity.Level
 import com.example.composition.domain.entity.Question
 import com.example.composition.domain.repository.GameRepository
+import java.lang.Integer.min
+import java.lang.Math.max
+import kotlin.random.Random
 
 object GameRepositoryImpl: GameRepository {
+
+    private const val MIN_SUM_VALUE = 2
+    private const val MIN_ANSWER_VALUE = 1
     override fun generateQuestion(maxValue: Int, countOption: Int): Question {
-        TODO("Not yet implemented")
+        val sum = Random.nextInt(MIN_SUM_VALUE, maxValue + 1)
+        val visibleNumber = Random.nextInt(MIN_ANSWER_VALUE, sum)
+        val options = HashSet<Int>()
+        val rightAnswer = sum - visibleNumber
+        options.add(rightAnswer)
+        val from = max(rightAnswer - countOption, MIN_ANSWER_VALUE)
+        val to = min(maxValue, rightAnswer + countOption)
+        while (options.size < countOption) {
+            options.add(Random.nextInt(from, to))
+        }
+        return Question(sum, options.toList(),visibleNumber )
     }
 
     override fun getGameSettings(level: Level): GameSettings {
